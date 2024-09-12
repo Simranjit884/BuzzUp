76fd9261db827b2da477e434541e2c3b3d5c550d
samy884 <ssgill884@gmail.com>
2024-09-12 21:16:54 +0530

    created an initial profile

--- lib/db.ts
+++ lib/db.ts
@@ -6,5 +6,6 @@ declare global {
 }
 
 export const db = globalThis.prisma || new PrismaClient();
-
+//globalThis doesnt get effected by hot reload, so it will stop creating a
+//new prismaclient connection everytime when there is a hot reload in the development mode.
 if (process.env.NODE_ENV !== "production") globalThis.prisma = db;

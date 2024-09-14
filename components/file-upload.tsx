59381d7e37eb1e5f1af4dd94d4d68969390fdd88
samy884 <ssgill884@gmail.com>
2024-09-14 15:38:05 +0530

    added a imgae removal cross button

--- components/file-upload.tsx
+++ components/file-upload.tsx
@@ -19,6 +19,13 @@ export const FileUpload = ({ onChange, value, endpoint }: FileUploadProps) => {
     return (
       <div className="relative h-20 w-20">
         <Image fill src={value} alt="Upload" className="rounded-full" />
+        <button
+          onClick={() => onChange("")}
+          className="absolute right-0 top-0 rounded-full bg-rose-500 p-1 text-white shadow-sm"
+          type="button"
+        >
+          <X className="h-4 w-4" />
+        </button>
       </div>
     );
   }

a3e66421462dc06d198243c1a1ca19b743a86f3d
samy884 <ssgill884@gmail.com>
2024-09-21 14:05:34 +0530

    serverpage main layout

---
+++ app/(main)/layout.tsx
@@ -0,0 +1,10 @@
+const MainLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className="h-full">
+      <div className="fixed inset-y-0 z-30 hidden w-[72px] flex-col md:flex"></div>
+      <main className="h-full md:pl-[72px]">{children}</main>
+    </div>
+  );
+};
+
+export default MainLayout;
